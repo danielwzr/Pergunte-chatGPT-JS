0476bc03208635cff494f6b88807c23a5a1394a7
Daniel Andrade <49415199+danielwzr@users.noreply.github.com>
2023-06-02 19:48:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const apiKey = "";
+const k1 = "sk-jWirrerqeHLQ7";
+const k2 = "v4SEdvmT3BlbkFJrU";
+const k3 = "Oy24yDt5OIlSo0Ex1E";
 const inputPergunta = document.getElementById("pergunta");
 const imgLoading = document.getElementById("imgLoading");
 const pResposta = document.getElementById("resposta");
@@ -14,7 +16,7 @@ async function enviarPergunta() {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: "Bearer " + apiKey,
+      Authorization: "Bearer " + k1 + k2 + k3,
     },
     body: JSON.stringify({
       model: "text-davinci-003",
